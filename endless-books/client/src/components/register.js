17625148ab2fefded34d97382c405b10ecc2ccd9
aurumbright <104322771+aurumbright@users.noreply.github.com>
2022-11-17 20:30:11 -0800

    Update register.js

--- endless-books/client/src/components/register.js
+++ endless-books/client/src/components/register.js
@@ -2,10 +2,6 @@ import { useState } from "react";
 import Input from "../components/Input";
 
 export default function Register() {
-<<<<<<< Updated upstream:endless-books/client/src/components/register.js
-    return <div>Register</div>;
-  }
-=======
   const [username, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -22,4 +18,3 @@ export default function Register() {
     </form>
   );
 }
->>>>>>> Stashed changes:client/src/pages/Register.js
