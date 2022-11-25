a0d95a27a423690773dd8a2f30cff1756c6ab7b5
Satorrid <Devintsefton@gmail.com>
2022-11-25 12:52:12 -0800

    some css

--- client/src/features/SetNav.js
+++ client/src/features/SetNav.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Nav from "./Nav";
-import NavBar from "../components/Navbar";
-import App from '../App';
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import Nav from "./Nav";
+// import NavBar from "../components/Navbar";
+// import App from "../App";
 
-const SetNav = (props) => {
+// const SetNav = (props) => {
+//   const isLoggedIn = props.isLoggedIn;
 
-    const isLoggedIn = props.isLoggedIn;
+//   if (isLoggedIn) {
+//     return <NavBar />;
+//   }
+// };
 
-    if (isLoggedIn) { 
-        return <NavBar />; 
-    } return <Nav />;
-}
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// // Try changing to isLoggedIn={true}:
+// root.render(<App isLoggedIn={false} />);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-// Try changing to isLoggedIn={true}:
-root.render(<App isLoggedIn={false} />);
-
-export default SetNav;
+// export default SetNav;
