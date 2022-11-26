07cc8ce5193413a6c4f9cbcce5f1b4b48b01f495
Satorrid <Devintsefton@gmail.com>
2022-11-25 16:52:28 -0800

    Feature(graph): setup for client with mutation for login working

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3001/",
+  uri: "http://localhost:3001/graphql",
   cache: new InMemoryCache(),
 });
 
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,10 +1,18 @@
-
+import { gql } from "@apollo/client";
 // server (computer, somewheree, facebook, apple, gmail, itunes)
 // store, crud => create, read, update, delete
 // creat, update,delete => graphql mutation
 // read query
 
 // client (desktop,laptop,mobile, browser)
-// const loginMutation = gql`
-
-`
+export const LOGIN_MUTATION = gql`
+  mutation Login($password: String, $email: String) {
+    login(password: $password, email: $email) {
+      token
+      user {
+        _id
+        email
+      }
+    }
+  }
+`;
